7a577539d1b6401825afc540b5bfd4ba90b4c3e3
Gonzalo Negro <gonzalonnegro@gmail.com>
2022-09-22 10:34:40 -0300

    Correcciones conexion

--- Sistemas/consulta/consultadetallemon.php
+++ Sistemas/consulta/consultadetallemon.php
@@ -40,11 +40,11 @@ function ConsultarIncidente($no_tic)
 	<meta charset="utf-8">
     <title>DETALLES DEL MONITOR</title>
     <link rel="icon" href="../imagenes/logoObrasPúblicas.png">
-	<link rel="stylesheet" type="text/css" href="../estilos/estiloconsultadetallemon.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../estilos/estiloconsultadetallemon.css">
 	<style>
 			body{
 			background-color: #edf0f5;
--- Sistemas/consulta/monitoresmov.php
+++ Sistemas/consulta/monitoresmov.php
@@ -1,7 +1,7 @@
 <?php 
 error_reporting(0);
 session_start();
-include('conexion.php');
+include('../particular/conexion.php');
 
 $consulta = ConsultarIncidente($_GET['no']);
 
@@ -31,7 +31,7 @@ function ConsultarIncidente($no_tic)
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" type="text/css" href="estiloconsultadetalleinv.css">
+    <link rel="stylesheet" type="text/css" href="../estilos/estiloconsultadetalleinv.css">
 	<style>
 			body{
 			background-color: #edf0f5;
